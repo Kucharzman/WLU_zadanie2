344a0498c63621ca9959125ef175f8baf48d28d6
Dejwa <visualstudent@zschie.edu.pl>
2022-10-04 13:59:43 +0200

    1.2 Login GUI

--- WLU_zadanie2/formLogowanie.Designer.cs
+++ WLU_zadanie2/formLogowanie.Designer.cs
@@ -28,19 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbVeri = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bttLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(157, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Użytkownik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(226, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasło";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(358, 77);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(244, 23);
+            this.tbUser.TabIndex = 2;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(358, 119);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(244, 23);
+            this.tbPass.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(152, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 34);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Weryfikacja";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(201, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(367, 14);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Wpisz znaki, które widzisz na poniższym obrazku aby kontynuować";
+            // 
+            // tbVeri
+            // 
+            this.tbVeri.Location = new System.Drawing.Point(358, 308);
+            this.tbVeri.Name = "tbVeri";
+            this.tbVeri.Size = new System.Drawing.Size(244, 23);
+            this.tbVeri.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WLU_zadanie2.Properties.Resources._6;
+            this.pictureBox1.Location = new System.Drawing.Point(191, 300);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 31);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(358, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 14);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "losuj nowy";
+            // 
+            // bttLogin
+            // 
+            this.bttLogin.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bttLogin.Location = new System.Drawing.Point(566, 373);
+            this.bttLogin.Name = "bttLogin";
+            this.bttLogin.Size = new System.Drawing.Size(198, 38);
+            this.bttLogin.TabIndex = 9;
+            this.bttLogin.Text = "Zaloguj";
+            this.bttLogin.UseVisualStyleBackColor = true;
             // 
             // formLogowanie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttLogin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbVeri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "formLogowanie";
             this.Text = "Sekretariat";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tbUser;
+        private TextBox tbPass;
+        private Label label3;
+        private Label label4;
+        private TextBox tbVeri;
+        private PictureBox pictureBox1;
+        private Label label5;
+        private Button bttLogin;
     }
 }
