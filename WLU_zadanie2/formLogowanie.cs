59aa87ed641961f7280fa69a703c198094f8a72a
Dejwa <visualstudent@zschie.edu.pl>
2022-10-04 14:23:57 +0200

    2.1 Logging in done

--- WLU_zadanie2/formLogowanie.cs
+++ WLU_zadanie2/formLogowanie.cs
@@ -2,6 +2,9 @@ namespace WLU_zadanie2
 {
     public partial class formLogowanie : Form
     {
+
+        public string sCorrVeri = "befhd";
+
         public formLogowanie()
         {
             InitializeComponent();
@@ -9,10 +12,10 @@ namespace WLU_zadanie2
 
         private void bttLogin_Click(object sender, EventArgs e)
         {
-            string sUser = tbUser.Text;
-            string sPass = tbPass.Text;
-            string sVeri = tbVeri.Text;
-            string sPicName = picVeri.
+            string sUser = tbUser.Text.Trim();
+            string sPass = tbPass.Text.Trim();
+            string sVeri = tbVeri.Text.Trim();
+
             bool bLogged = false;
             bool bVerified = false;
 
@@ -22,13 +25,65 @@ namespace WLU_zadanie2
                 bLogged = true;
             }
 
-            
+            if (sVeri == sCorrVeri)
+            {
+                bVerified = true;
+            }
 
+
+            if (bLogged == true & bVerified == true)
+            {
+                MessageBox.Show("Gituwa", "Bien");
+            }
+            else
+            {
+                MessageBox.Show("Ah kogut", "No bien");
+            }
 
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
+            string[] sciezki = Directory.GetFiles(@"C:\Users\student\source\repos\WLU_zadanie2\WLU_zadanie2", "*.png");
+            List<string> zdjecia = sciezki.ToList();
+
+            Random kotlet = new Random();
+
+            picVeri.ImageLocation = sciezki[kotlet.Next(0, zdjecia.Count - 1)];
+
+            if (picVeri.ImageLocation.Contains("1.png"))
+            {
+                sCorrVeri = "mxyxw";
+            } else
+            if (picVeri.ImageLocation.Contains("2.png"))
+            {
+                sCorrVeri = "b5nmm";
+            }
+            else
+            if (picVeri.ImageLocation.Contains("3.png"))
+            {
+                sCorrVeri = "74853";
+            }
+            else
+            if (picVeri.ImageLocation.Contains("4.png"))
+            {
+                sCorrVeri = "cg5dd";
+            }
+            else
+            if (picVeri.ImageLocation.Contains("5.png"))
+            {
+                sCorrVeri = "x3deb";
+            }
+            else
+            if (picVeri.ImageLocation.Contains("6.png"))
+            {
+                sCorrVeri = "befhd";
+            }
+            else
+            if (picVeri.ImageLocation.Contains("7.png"))
+            {
+                sCorrVeri = "rc7gb3";
+            }
 
         }
     }
